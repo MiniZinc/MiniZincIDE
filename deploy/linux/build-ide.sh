7db533a267a52cca313656df6dc63b3bcbc578e9
Guido Tack <guido.tack@monash.edu>
2014-02-03 16:16:02 +1100

    Simplified

--- deploy/linux/build-ide.sh
+++ deploy/linux/build-ide.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 VERSION=0.9.2
 PACKAGE=MiniZincIDE-$VERSION-linux-x86_64
+QTDIR=~/Qt5.2.0/5.2.0/gcc_64
+export PATH=$QTDIR/bin:$PATH
 rm -rf build-ide
 mkdir build-ide
 cd build-ide
@@ -15,9 +17,9 @@ mkdir $PACKAGE/lib && \
 for q in \
   libicui18n.so.51 libicuuc.so.51 libicudata.so.51 \
   libQt5Core.so.5 libQt5DBus.so.5 libQt5Gui.so.5 libQt5Widgets.so.5; do \
-  cp ~/Qt5.2.0/5.2.0/gcc_64/lib/$q $PACKAGE/lib/; \
+  cp $QTDIR/$q $PACKAGE/lib/; \
 done && \
-cp -r ~/Qt5.2.0/5.2.0/gcc_64/plugins $PACKAGE/ && \
+cp -r $QTDIR/plugins $PACKAGE/ && \
 cp ../../MiniZincIDE.sh $PACKAGE/ && \
 chmod +x $PACKAGE/MiniZincIDE.sh && \
 tar czf $PACKAGE.tgz $PACKAGE && \
