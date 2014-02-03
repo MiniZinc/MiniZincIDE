99528ffeec04761f7e2bb9cd3a07f08c5a170651
Guido Tack <guido.tack@monash.edu>
2014-02-03 16:37:35 +1100

    Add option to archive release branch

--- deploy/archive-ide.sh
+++ deploy/archive-ide.sh
@@ -5,10 +5,13 @@ cd archive-ide
 git clone http://www.minizinc.org/repos/MiniZincIDE
 cd MiniZincIDE
 if [ "$1" == "rotd" ]; then git checkout develop; fi
+if [ "$1" == "release" ]; then git checkout release/$2; fi
 TAG=`git describe --always`
 DATE=`date "+%Y-%m-%d"`
 if [ "$1" == "rotd" ]
 then VERSION=${DATE}-${TAG}
+elif [ "$1" == "release" ]
+then VERSION=$2
 else VERSION=${TAG}
 fi
 git archive --prefix=MiniZincIDE-${VERSION}/ --output=MiniZincIDE-${VERSION}.tgz HEAD:MiniZincIDE/
