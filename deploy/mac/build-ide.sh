e210baa8acb2973ddb6b3480fbbde1cc69561fd8
Guido Tack <guido.tack@monash.edu>
2014-02-03 16:36:59 +1100

    Mac packaging script

--- deploy/mac/build-ide.sh
+++ deploy/mac/build-ide.sh
@@ -3,10 +3,13 @@ VERSION=0.9.2
 rm -rf build-ide
 mkdir build-ide
 cd build-ide
-wget http://www.minizinc.org/download/MiniZincIDE-$(VERSION).tar.gz && \
-tar xzf MiniZincIDE-$(VERSION).tar.gz && \
-cd MiniZincIDE-$(VERSION) && \
+wget http://www.minizinc.org/downloads/MiniZincIDE/MiniZincIDE-$VERSION.tgz && \
+tar xzf MiniZincIDE-$VERSION.tgz && \
+cd MiniZincIDE-$VERSION && \
 qmake -makefile && \
 make -j4 && \
 macdeployqt MiniZincIDE.app -dmg && \
-mv MiniZincIDE.dmg MiniZincIDE-$(VERSION).dmg
+mv MiniZincIDE.dmg ../../MiniZincIDE-$VERSION.dmg &&
+cd ../.. && \
+rm -r build-ide
+
