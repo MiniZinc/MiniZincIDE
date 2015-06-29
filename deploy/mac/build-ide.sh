a6407edba8643a94719369989b19d510d3df7efe
Guido Tack <guido.tack@monash.edu>
2015-06-29 18:38:56 +1000

    Build scripts

--- deploy/mac/build-ide.sh
+++ deploy/mac/build-ide.sh
@@ -4,18 +4,19 @@ mkdir build-ide
 if [ "x$2" == "x" ]; then
   VERSION=$1
   cd build-ide
-  wget http://www.minizinc.org/downloads/MiniZincIDE/MiniZincIDE-$VERSION.tgz
+  curl -L -O https://github.com/MiniZinc/MiniZincIDE/archive/$VERSION.tar.gz
+  mv $VERSION.tar.gz MiniZincIDE-$VERSION.tgz
 else
   VERSION=$2
   cp $1 build-ide/MiniZincIDE-$VERSION.tgz
   cd build-ide
 fi
 tar xzf MiniZincIDE-$VERSION.tgz && \
-cd MiniZincIDE-$VERSION && \
+cd MiniZincIDE-$VERSION/MiniZincIDE && \
 qmake -makefile && \
 make -j4 && \
 macdeployqt MiniZincIDE.app -dmg && \
-mv MiniZincIDE.dmg ../../MiniZincIDE-$VERSION.dmg &&
-cd ../.. && \
+mv MiniZincIDE.dmg ../../../MiniZincIDE-$VERSION.dmg &&
+cd ../../.. && \
 rm -r build-ide
 
