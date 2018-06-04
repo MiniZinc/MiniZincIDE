3b0dbca486bce8e4a759abd46c448c93a003fae5
Guido Tack <guido.tack@monash.edu>
2018-06-05 09:19:08 +1000

    Add support for selecting compiler optimisation level (-O0..-O5)

--- MiniZincIDE/solverconfiguration.cpp
+++ MiniZincIDE/solverconfiguration.cpp
@@ -19,7 +19,7 @@ QVector<SolverConfiguration> SolverConfiguration::defaultConfigs(const QVector<S
     def.clearOutputWindow = false;
     def.verboseFlattening = false;
     def.flatteningStats = false;
-    def.optimizedFlattening = true;
+    def.optimizationLevel = 1;
     def.additionalData = "";
     def.additionalCompilerCommandline = "";
     def.nThreads = 1;
@@ -54,7 +54,7 @@ bool SolverConfiguration::operator==(const SolverConfiguration &sc) const
             sc.clearOutputWindow==clearOutputWindow &&
             sc.verboseFlattening==verboseFlattening &&
             sc.flatteningStats==flatteningStats &&
-            sc.optimizedFlattening==optimizedFlattening &&
+            sc.optimizationLevel==optimizationLevel &&
             sc.additionalData==additionalData &&
             sc.additionalCompilerCommandline==additionalCompilerCommandline &&
             sc.nThreads==nThreads &&
