9704f99edb6bbcc63974392d7513e4f04650b42f
Jason N <admin@cyderize.org>
2020-09-18 15:14:57 +1000

    Also use dark mode theme on linux

--- MiniZincIDE/mainwindow.cpp
+++ MiniZincIDE/mainwindow.cpp
@@ -2339,14 +2339,14 @@ void MainWindow::on_actionDark_mode_toggled(bool enable)
     static_cast<CodeEditor*>(IDE::instance()->cheatSheet->centralWidget())->setDarkMode(darkMode);
 
     if (darkMode) {
-#ifdef Q_OS_WIN
+#ifndef Q_OS_MAC
         QFile sheet(":/dark_mode.css");
         sheet.open(QFile::ReadOnly);
         qApp->setStyleSheet(sheet.readAll());
 #endif
         ui->outputConsole->document()->setDefaultStyleSheet(".mznnotice { color : #13C4F5 }");
     } else {
-#ifdef Q_OS_WIN
+#ifndef Q_OS_MAC
         qApp->setStyleSheet("");
 #endif
         ui->outputConsole->document()->setDefaultStyleSheet(".mznnotice { color : blue }");
