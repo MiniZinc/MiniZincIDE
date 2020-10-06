4544c67c6160064f0127d17c5585d5f61a5401ce
Jason N <admin@cyderize.org>
2020-10-06 11:57:27 +1100

    Add fallback for when libnss3 is missing

--- resources/scripts/MiniZincIDE.sh
+++ resources/scripts/MiniZincIDE.sh
@@ -3,4 +3,9 @@ DIR=$(dirname $(readlink -f "$0"))
 [ -n "$XDG_RUNTIME_DIR" ] && mkdir -p $XDG_RUNTIME_DIR -m 700
 export LD_LIBRARY_PATH="$DIR"/lib:$LD_LIBRARY_PATH
 export QT_PLUGIN_PATH="$DIR"/plugins
+
+if ldd "$DIR"/bin/MiniZincIDE | grep -q 'libnss3.so => not found' ; then
+    export LD_LIBRARY_PATH="$DIR"/lib_fallback:$LD_LIBRARY_PATH
+fi
+
 exec "$DIR"/bin/MiniZincIDE $@
